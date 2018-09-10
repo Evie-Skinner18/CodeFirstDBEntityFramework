3ccd26f7d83aa603ac42f6c59bf98f9ea8ca416d
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-09-10 17:09:52 +0100

    Added DB context in Data dir

---
+++ ConsoleApp2/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodeFirstDB.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace CodeFirstDB.Data
+{
+    class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
--- ConsoleApp2/Models/BlogModel.cs
+++ ConsoleApp2/Models/BlogModel.cs
@@ -1,6 +1,11 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace CodeFirstDB.Models
 {
     class BlogModel
     {
@@ -14,13 +19,20 @@ namespace ConsoleApp2
     {
         public int BlogId { get; set; }
         public string Name { get; set; }
+
+        // virtual method that returns a list of Posts. Declaring it as virtual because we might want to override it later
         public virtual List<Post> Posts { get; set; }
     }
 
     public class Post
     {
         public int PostId { get; set; }
-        public string PostName { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+
+        // virtual method that returns a Blog object called Blog. Declaring it as virtual because we might want to override it later
+        public virtual Blog Blog { get; set; }
     }
   
 }
