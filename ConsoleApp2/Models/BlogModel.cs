20f06f1a52b814b697632ce9ad95583cc0e3a3a9
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-09-10 16:47:54 +0100

    Added readme

---
+++ ConsoleApp2/Models/BlogModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class BlogModel
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string PostName { get; set; }
+    }
+  
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# Code-First Database Fun
+
+## Tools and Technologies
+- C#
+- .Net
+- Entity Framework
+- SQL Server Management Studio
+
+
+## Happy coding!   <3
