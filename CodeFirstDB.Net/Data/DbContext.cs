bb5fc9620cd8428c8c51ab38f48d5f47f1031afe
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-09-11 09:25:38 +0100

    Completed basic version of .Net app

---
+++ CodeFirstDB.Net/Data/DbContext.cs
@@ -0,0 +1,15 @@
+﻿using CodeFirstDB.Net.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Text;
+
+namespace CodeFirstDB.Net.Data
+{
+    class BloggingContext : DbContext
+    {
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Post> Posts { get; set; }
+
+    }
+}
---
+++ CodeFirstDB.Net/Models/Blog.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstDB.Net.Data;
+
+namespace CodeFirstDB.Net.Models
+{
+    class BlogModel
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new BloggingContext())
+            {
+                // prompt the user to create a new blog. This will be added to the DB
+                Console.Write("Enter a name for a new blog");
+                var name = Console.ReadLine();
+
+                var blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+
+                // Select each blog in Blogs
+                var query = from b in db.Blogs
+                            orderby b.Name
+                            select b;
+
+                // loop through the array of blogs that query returns and display each one's name to the user
+                Console.WriteLine("All blogs from the database: ");
+                foreach (var i in query)
+                {
+                    Console.WriteLine(i.Name);
+                }
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+
+
+            }
+
+        }
+    }
+
+    public class Blog
+    {
+        public int BlogId { get; set; }
+        public string Name { get; set; }
+
+        // virtual method that returns a list of Posts. Declaring it as virtual because we might want to override it later
+        public virtual List<Post> Posts { get; set; }
+    }
+
+    public class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public int BlogId { get; set; }
+
+        // virtual method that returns a Blog object called Blog. Declaring it as virtual because we might want to override it later
+        public virtual Blog Blog { get; set; }
+    }
+
+}
