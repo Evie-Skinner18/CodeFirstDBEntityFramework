25e36d44306e1de489fda65db148254e8713d6e1
Evie Skinner <evie.skinner@gwle.onmicrosoft.com>
2018-09-12 17:17:17 +0100

    Changed the model with a migration so DisplayName is now display_name

--- CodeFirstDB.Net/Data/DbContext.cs
+++ CodeFirstDB.Net/Data/DbContext.cs
@@ -11,5 +11,14 @@ namespace CodeFirstDB.Net.Data
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .Property(u => u.DisplayName)
+                .HasColumnName("display_name");
+        }
     }
+
+    
 }
